5fa8056f3f27a227ecce36ee26f16953d530c5f6
“leandrokaminise” <leandrotetsuo98@gmail.com>
2023-12-20 09:42:35 -0300

    corrected some errors

--- script.js
+++ script.js
@@ -2,27 +2,26 @@ function toggleMode() {
   const html = document.documentElement
   html.classList.toggle("light")
 
-  //pegar a tag img
+  // pegar a tag img
   const img = document.querySelector("#profile img")
 
-  //substituir a imagem
+  // substituir a imagem
   if (html.classList.contains("light")) {
-    // se tiver light mode adicionar a imagem light
+    // se tiver light mode, adicionar a imagem light
     img.setAttribute("src", "./assets/avatar-light.png")
   } else {
-    // se tiver sem light mode manter a imagem normal
+    // set tiver sem light mode, manter a imagem normal
     img.setAttribute("src", "./assets/avatar.png")
   }
-
-  if (html.classList.contains("light")) {
-    img.setAttribute(
-      "alt",
-      "Foto de Leandro Tetsuo, sorrindo, usando boné, de barba e céu azul de fundo."
-    )
-  } else {
-    img.setAttribute(
-      "alt",
-      "Foto de Leandro Tetsuo, sério, usando camisa verde, barba, topete e natureza com céu azul de fundo."
-    )
-  }
+}
+if (html.classList.contains("light")) {
+  img.setAttribute(
+    "alt",
+    "Foto de Leandro Tetsuo, sorrindo, usando boné, de barba e céu azul de fundo."
+  )
+} else {
+  img.setAttribute(
+    "alt",
+    "Foto de Leandro Tetsuo, sério, usando camisa verde, barba, topete e natureza com céu azul de fundo."
+  )
 }
